20bd575ae592c8b37f3219e8c677d3b8c453dea7
Mike Pinna <36490042+Pinna3@users.noreply.github.com>
2021-02-01 14:22:58 -0500

    Update tradingSandbox.py

--- tradingSandbox.py
+++ tradingSandbox.py
@@ -30,3 +30,35 @@ def accountAndPositions():
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # append lowerBB_securities with industry_category_subcategory items
+# for security in range(len(lowerBB_securities)):
+#     try:
+#         stock = Stock(lowerBB_securities[security][0], 'SMART', 'USD', primaryExchange = lowerBB_securities[security][1])
+#         details = ib.reqContractDetails(stock)
+#         details_dict = util.tree(details)
+#         lowerBB_securities[security].append(details_dict[0]['ContractDetails']['industry'])
+#         lowerBB_securities[security].append(details_dict[0]['ContractDetails']['category'])
+#         lowerBB_securities[security].append(details_dict[0]['ContractDetails']['subcategory'])
+#     except IndexError:
+#         lowerBB_securities[security].append('')
+#         lowerBB_securities[security].append('')
+#         lowerBB_securities[security].append('')
+#     except KeyError:
+#         lowerBB_securities[security].append('')
+#         lowerBB_securities[security].append('')
+#         lowerBB_securities[security].append('')
+# print(lowerBB_securities)
