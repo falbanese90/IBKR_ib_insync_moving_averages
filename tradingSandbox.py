a5f0e7ea881697e46c4fc1005e26ff0dc286c738
Mike Pinna <36490042+Pinna3@users.noreply.github.com>
2021-02-01 14:22:25 -0500

    Update tradingSandbox.py

--- tradingSandbox.py
+++ tradingSandbox.py
@@ -4,59 +4,29 @@ from stock_buckets import *
 
 # connect to IBKR api
 ib = IB()
-ib.connect('127.0.0.1', 4001, clientId=1)
+ib.connect('127.0.0.1', 4001, clientId=2)
+
+# create dictionary of account and portfolio values for risk management
+def accountAndPositions():
+    acct = util.tree(ib.accountSummary())
+    acctSum = {}
+    for index in (20, 9, 13, 32):
+        acctSum[acct[index]['tag']] = acct[index]['value']
+    positions = util.tree(ib.positions())
+    acctSum['Positions'] = acctPos = {}
+    for index in range(len(positions)):
+        acctPos[positions[index]['contract']['Option']['symbol']] = [
+            positions[index]['contract']['Option']['secType'],
+            positions[index]['contract']['Option']['strike'],
+            positions[index]['contract']['Option']['right'],
+            positions[index]['contract']['Option']['lastTradeDateOrContractMonth']
+        ]
+    return acctSum
 
 # googl = Stock('GOOGL', 'SMART', 'USD', primaryExchange = 'NASDAQ')
 # details = ib.reqContractDetails(googl)
 # details_dict = util.tree(details)
 # print(details_dict)
 
-# broad account metrics
-acct = util.tree(ib.accountSummary())
-acctSum = {}
-for index in (20, 9, 13, 32):
-    acctSum[acct[index]['tag']] = acct[index]['value']
-print(acctSum)
-
-# portfolio metrics
-positions = util.tree(ib.positions())
-# print(positions)
-acctSum['Positions'] = acctPos = {}
-for index in range(len(positions)):
-    acctPos[positions[index]['contract']['option']['symbol']] = positions[index]['contract']['option']['secType']
-print(acctSum)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # append lowerBB_securities with industry_category_subcategory items
-# for security in range(len(lowerBB_securities)):
-#     try:
-#         stock = Stock(lowerBB_securities[security][0], 'SMART', 'USD', primaryExchange = lowerBB_securities[security][1])
-#         details = ib.reqContractDetails(stock)
-#         details_dict = util.tree(details)
-#         lowerBB_securities[security].append(details_dict[0]['ContractDetails']['industry'])
-#         lowerBB_securities[security].append(details_dict[0]['ContractDetails']['category'])
-#         lowerBB_securities[security].append(details_dict[0]['ContractDetails']['subcategory'])
-#     except IndexError:
-#         lowerBB_securities[security].append('')
-#         lowerBB_securities[security].append('')
-#         lowerBB_securities[security].append('')
-#     except KeyError:
-#         lowerBB_securities[security].append('')
-#         lowerBB_securities[security].append('')
-#         lowerBB_securities[security].append('')
-# print(lowerBB_securities)
