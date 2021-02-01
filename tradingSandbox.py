e82dd1c0b720ca1533cd30075d68353c7faa7eef
Mike Pinna <36490042+Pinna3@users.noreply.github.com>
2021-01-31 20:27:55 -0500

    Update tradingSandbox.py
    creating account / portfolio metric dictionaries for risk management considerations

--- tradingSandbox.py
+++ tradingSandbox.py
@@ -6,10 +6,40 @@ from stock_buckets import *
 ib = IB()
 ib.connect('127.0.0.1', 4001, clientId=1)
 
-googl = Stock('GOOGL', 'SMART', 'USD', primaryExchange = 'NASDAQ')
-details = ib.reqContractDetails(googl)
-details_dict = util.tree(details)
-print(details_dict)
+# googl = Stock('GOOGL', 'SMART', 'USD', primaryExchange = 'NASDAQ')
+# details = ib.reqContractDetails(googl)
+# details_dict = util.tree(details)
+# print(details_dict)
+
+# broad account metrics
+acct = util.tree(ib.accountSummary())
+acctSum = {}
+for index in (20, 9, 13, 32):
+    acctSum[acct[index]['tag']] = acct[index]['value']
+print(acctSum)
+
+# portfolio metrics
+positions = util.tree(ib.positions())
+# print(positions)
+acctSum['Positions'] = acctPos = {}
+for index in range(len(positions)):
+    acctPos[positions[index]['contract']['option']['symbol']] = positions[index]['contract']['option']['secType']
+print(acctSum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # # append lowerBB_securities with industry_category_subcategory items
